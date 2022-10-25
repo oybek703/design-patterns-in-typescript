14f4a9bf2ae5805cbc17056e847519d2e6ec8b19
Yoriqulov Oybek <hhoybek@gmail.com>
2022-10-25 23:24:31 +0500

    enums

--- app.ts
+++ app.ts
@@ -1,12 +1,30 @@
-const skill: readonly [number, string] = [1, 'Dev']
+enum StatusCode {
+    SUCCESS,
+    FAILED = 'F',
+    IN_PROCESS = 'P'
+}
 
-skill.push('test')
+function action(status: StatusCode) {
 
-const skills: readonly string[] = ['Dev', 'DevOps']
-const skills1: Array<string> = ['Dev', 'DevOps']
-const skills2: ReadonlyArray<string> = ['Dev', 'DevOps']
+}
 
-skills[1] = 'test'
 
-skills1[0] = 'asdasd'
-skills2[0] = 'asdasd'
+action(1)
+action(3)
+action('F')
+
+function compute() {
+    return 100
+}
+
+enum Roles {
+    USER = 1,
+    ADMIN = USER * 10,
+    MODERATOR = compute()
+}
+
+function test(x: { ADMIN: number }) {
+
+}
+
+test(Roles)
