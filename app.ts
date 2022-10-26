9628d36991fac3969c1cfac37248276ddffde684
Yoriqulov Oybek <hhoybek@gmail.com>
2022-10-26 17:01:44 +0500

    class method override

--- app.ts
+++ app.ts
@@ -1,18 +1,12 @@
-class User {
-    skills: string[]
 
-    addSkill(skills: string[]): void
-    addSkill(skills: string): void
-    addSkill(skills: string[] | string): void {
-        if (typeof skills === 'string') {
-            this.skills.push(skills)
-        } else {
-            skills.forEach(skill => this.skills.push(skill))
-        }
+class Payment {
+    protected pay() {
+        return ''
     }
 }
 
-const user = new User()
-user.addSkill('testing')
-user.addSkill('devops')
-user.addSkill(['s1', 's2'])
+class Payed extends Payment{
+    protected override pay() {
+        return  ''
+    }
+}
