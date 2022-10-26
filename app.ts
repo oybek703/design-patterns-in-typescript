d41ea5186e3085f01d79c40e8394ba563a6a9804
Yoriqulov Oybek <hhoybek@gmail.com>
2022-10-26 12:16:06 +0500

    never

--- app.ts
+++ app.ts
@@ -1,4 +1,26 @@
-type log = () => void
+type paymentAction = 'refund' | 'checkout' | 'reject'
 
+function processAction(action: paymentAction) {
+    switch (action) {
+        case 'checkout':
+            break
+        case 'refund':
+            break
+        default:
+            const _: never = action
+            throw new Error('Invalid action type!')
+    }
+}
 
-const l1: log = () => true
+function generateError(): never {
+    throw new Error('Error occurred!')
+}
+
+function getResult(x : number | string): boolean {
+    if (typeof x === 'number') {
+        return true
+    } else if (typeof x === 'string') {
+        return false
+    }
+    // generateError()
+}
