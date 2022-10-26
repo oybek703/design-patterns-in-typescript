a6d1230b153d85dae66328bf57ea8f0901692eb6
Yoriqulov Oybek <hhoybek@gmail.com>
2022-10-26 15:01:59 +0500

    multiple constructors

--- app.ts
+++ app.ts
@@ -1,9 +1,21 @@
 class User {
     name: string
+    age: number
 
-    constructor(name: string) {
-        this.name = name
+    constructor()
+    constructor(name: string)
+    constructor(age: number)
+    constructor(nameOrAge?: string | number, age?: number) {
+        if (typeof nameOrAge === 'string') {
+            this.name = nameOrAge
+        } else if (typeof nameOrAge === 'number') {
+            this.age = nameOrAge
+        } else if (typeof age === 'number') {
+            this.age = age
+        }
     }
 }
 
 const user = new User('John')
+const user1 = new User()
+const user2 = new User(23)
