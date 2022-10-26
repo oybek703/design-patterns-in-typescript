798a605284ceb95576fe7d9e5b79760b7f512748
Yoriqulov Oybek <hhoybek@gmail.com>
2022-10-26 09:32:35 +0500

    basics finished

--- app.ts
+++ app.ts
@@ -1,30 +1,4 @@
-enum StatusCode {
-    SUCCESS,
-    FAILED = 'F',
-    IN_PROCESS = 'P'
+const enum Roles {
+    AMDIN,
+    USER
 }
-
-function action(status: StatusCode) {
-
-}
-
-
-action(1)
-action(3)
-action('F')
-
-function compute() {
-    return 100
-}
-
-enum Roles {
-    USER = 1,
-    ADMIN = USER * 10,
-    MODERATOR = compute()
-}
-
-function test(x: { ADMIN: number }) {
-
-}
-
-test(Roles)
