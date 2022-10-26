741a280cd9026479178a7cd09da2df0654110b6a
Yoriqulov Oybek <hhoybek@gmail.com>
2022-10-26 10:56:45 +0500

    void

--- app.ts
+++ app.ts
@@ -1,32 +1,4 @@
-interface IData {
-    sum: number
-    from: number
-    to: number
-}
+type log = () => void
 
-interface IReq extends IData {
-}
 
-enum Status {
-    Success = 'status',
-    Failed = 'failed'
-}
-
-interface IResDataSuccess extends IData {
-    databaseId: number
-}
-
-interface IResDataFail {
-    errorMessage: string
-    errorCode: number
-}
-
-interface IResSuccess {
-    status: Status.Success,
-    data: IResDataSuccess
-}
-
-interface IResFail {
-    status: Status.Failed,
-    data: IResDataFail
-}
+const l1: log = () => true
