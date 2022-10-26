e031aa20bbb26b477c945e9a7bb4ac679b191760
Yoriqulov Oybek <hhoybek@gmail.com>
2022-10-26 14:17:38 +0500

    type guards

--- app.ts
+++ app.ts
@@ -1,26 +1,32 @@
-type paymentAction = 'refund' | 'checkout' | 'reject'
+interface User {
+    name: 'John'
+    email: 'test@gmail.com'
+}
 
-function processAction(action: paymentAction) {
-    switch (action) {
-        case 'checkout':
-            break
-        case 'refund':
-            break
-        default:
-            const _: never = action
-            throw new Error('Invalid action type!')
+interface Admin {
+    role: 0 | 1
+    name: 'Doe'
+}
+
+function isAdmin(user: User | Admin): user is Admin {
+    return 'role' in user
+}
+
+function setRoleZero(user: User | Admin): never {
+    if (isAdmin(user)) {
+        user.role = 0
+    }
+    throw new Error('User is not admin!')
+}
+
+function logId(x: string | number) {
+    if (isString(x)) {
+        const tempX = x
+    } else {
+
     }
 }
 
-function generateError(): never {
-    throw new Error('Error occurred!')
-}
-
-function getResult(x : number | string): boolean {
-    if (typeof x === 'number') {
-        return true
-    } else if (typeof x === 'string') {
-        return false
-    }
-    // generateError()
+function isString(x: string | number): x is string {
+    return typeof x === 'string'
 }
