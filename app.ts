2b7f7eb1f20142d7fc28435f2598485581ff35d7
Yoriqulov Oybek <hhoybek@gmail.com>
2022-10-27 15:15:06 +0500

    generic sort function

--- app.ts
+++ app.ts
@@ -1,17 +1,26 @@
-function toString<T>(data: T): string | undefined {
-    if (Array.isArray(data)) return data.toString()
-    switch (typeof data) {
-        case 'string':
-            return data
-        case 'bigint':
-        case 'boolean':
-        case 'number':
-        case 'symbol':
-        case 'function':
-            return data.toString()
-        default:
-            return undefined
-    }
+interface Data {
+    id: number
+    name: string
 }
 
-console.log(toString(null))
+type orderTypes = 'asc' | 'desc'
+
+function sortData<T extends Data>(data: Array<T>, order: orderTypes = 'asc'): Array<T> {
+    return data.sort((a, b) => order === 'asc' ? a.id - b.id : b.id - a.id)
+    // if (order === 'asc') {
+    //     return data.sort((a, b) => a.id - b.id)
+    // } else {
+    //     return data.sort((a, b) => b.id - a.id)
+    // }
+}
+
+const data: Data[] = [
+    {id: Math.round(Math.random() * 100), name: 'u1'},
+    {id: Math.round(Math.random() * 100), name: 'u2'},
+    {id: Math.round(Math.random() * 100), name: 'u3'},
+    {id: Math.round(Math.random() * 100), name: 'u4'}
+]
+
+console.log(data)
+console.log(sortData(data, 'desc'))
+console.log(sortData(data))
