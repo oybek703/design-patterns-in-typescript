dcfffe5022c915bcd9333d317cfa7a4faa30d5b2
Yoriqulov Oybek <hhoybek@gmail.com>
2022-10-27 16:14:09 +0500

    generics in new branch

--- app.ts
+++ app.ts
@@ -1,10 +1,41 @@
-class User {
-    constructor(private name: string) {
-    }
+type Constructor = new (...args: any[]) => {}
+type GConstructor<T> = new (...args: any[]) => T
 
-    logName() {
-        console.log(this.name)
+class List {
+    constructor(public items: string[]) {
     }
 }
 
-new User('John').logName()
+class Accordion {
+    constructor(public isOpened: boolean) {
+    }
+}
+
+type ListType = GConstructor<List>
+type AccordionType = GConstructor<Accordion>
+
+class ExtendedListClass extends List {
+    first() {
+        return this.items[0]
+    }
+}
+
+function ExtendedList<TBase extends ListType & AccordionType>(Base: TBase) {
+    return class ExtendedList extends Base {
+        first() {
+            return this.items[0]
+        }
+    }
+}
+
+class AccordionList {
+    isOpened: boolean
+
+    constructor(public items: string[]) {
+    }
+}
+
+const list = ExtendedList(AccordionList)
+
+const res = new list(['first item', 'second item'])
+console.log(res.first())
