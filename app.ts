d6bc3a042b00babd6edc266e8f09331117224583
Yoriqulov Oybek <hhoybek@gmail.com>
2022-10-27 22:30:21 +0500

    indexed access

--- app.ts
+++ app.ts
@@ -1,34 +1,15 @@
-class Data {
-    group: number
+const roles = ['user', 'admin', 'moderator'] as const
+
+type rolesType = typeof roles[number]
+
+
+interface Role {
     name: string
 }
 
-const data: Data[] = [
-    {group: 1, name: 'a'},
-    {group: 1, name: 'b'},
-    {group: 2, name: 'a'},
-    {group: 1, name: 'c'},
-    {group: 3, name: 'a'},
-    {group: 2, name: 'b'}
-]
-
-interface IGroup<T> {
-    [key: string]: T[]
+interface User {
+    name: string
+    roles: Role[]
 }
 
-function groupData<T extends Record<string, any>>(data: T[], key: keyof T): IGroup<T> {
-    const res: IGroup<T> = {}
-    data.forEach(value => {
-        const resKey = value[key]
-        if (!res[resKey]) {
-            res[resKey] = []
-            res[resKey].push(value)
-        } else {
-            res[resKey].push(value)
-        }
-    })
-    return res
-}
-
-console.log(groupData(data, 'group'))
-console.log(groupData(data, 'name'))
+type roleType = User['roles'][number]
