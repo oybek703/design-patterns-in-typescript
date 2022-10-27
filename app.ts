447d7b1c948ceccf4605bcec0d2e4ea908207c3c
Yoriqulov Oybek <hhoybek@gmail.com>
2022-10-27 14:37:50 +0500

    basic JSON.stringify done

--- app.ts
+++ app.ts
@@ -1,11 +1,5 @@
-const nums: Array<number> = [1, 2, 3, 4]
-
-async function test() {
-    const a = await new Promise<string>(resolve => {
-        resolve('Some value')
-    })
+function toString<T>(data: T): string | undefined {
+    return JSON.stringify(data)
 }
 
-const skills: Record<string, boolean> = {
-    drive: true
-}
+const test1 = JSON.stringify(undefined)
