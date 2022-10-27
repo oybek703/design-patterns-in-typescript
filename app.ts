87a478475664545dc7b00b2b4647f9acd861a4a4
Yoriqulov Oybek <hhoybek@gmail.com>
2022-10-27 16:16:09 +0500

    generics finish

--- app.ts
+++ app.ts
@@ -1,41 +1,11 @@
-type Constructor = new (...args: any[]) => {}
-type GConstructor<T> = new (...args: any[]) => T
+const nums: Array<number> = [1, 2, 3, 4]
 
-class List {
-    constructor(public items: string[]) {
-    }
+async function test() {
+    const a = await new Promise<string>(resolve => {
+        resolve('Some value')
+    })
 }
 
-class Accordion {
-    constructor(public isOpened: boolean) {
-    }
+const skills: Record<string, boolean> = {
+    drive: true
 }
-
-type ListType = GConstructor<List>
-type AccordionType = GConstructor<Accordion>
-
-class ExtendedListClass extends List {
-    first() {
-        return this.items[0]
-    }
-}
-
-function ExtendedList<TBase extends ListType & AccordionType>(Base: TBase) {
-    return class ExtendedList extends Base {
-        first() {
-            return this.items[0]
-        }
-    }
-}
-
-class AccordionList {
-    isOpened: boolean
-
-    constructor(public items: string[]) {
-    }
-}
-
-const list = ExtendedList(AccordionList)
-
-const res = new list(['first item', 'second item'])
-console.log(res.first())
