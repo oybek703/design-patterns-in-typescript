8f7ac1f79c119aac35466fb943d4b98ed43977f5
Yoriqulov Oybek <hhoybek@gmail.com>
2022-10-27 10:52:13 +0500

    this in classes

--- app.ts
+++ app.ts
@@ -1,9 +1,28 @@
-class UserServices {
-    private static db: any
+class Payment {
+    date: Date = new Date()
 
-    static {
-        this.db = 'test001'
+    getDate(this: Payment, title: string = 'Date') {
+        return `${title} => ${this.date}`
+    }
+
+    getDateArrow = () => {
+        return this.date
     }
 }
 
-console.log(UserServices)
+const p1 = new Payment()
+
+const user = {
+    id: 1,
+    paymentDate: p1.getDate.bind(p1)
+}
+
+class PaymentServices extends Payment{
+    getInfo() {
+        console.log(this)
+        return super.getDateArrow()
+    }
+}
+
+console.log(user.paymentDate())
+console.log(new PaymentServices().getInfo())
