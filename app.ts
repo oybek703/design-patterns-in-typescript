26a51c4322192fec60225fe5a2d670da6cd29925
Yoriqulov Oybek <hhoybek@gmail.com>
2022-10-27 10:07:36 +0500

    cart self implementation done

--- app.ts
+++ app.ts
@@ -1,12 +1,48 @@
+class Delivery {
+    date: Date
+    address?: string
+    shopId: string
 
-class Payment {
-    protected pay() {
-        return ''
+    constructor(date: Date, address?: string, shopId?: string) {
+        this.date = date
+        if (address) {
+            this.address = address
+        }
+        if (shopId) {
+            this.shopId = shopId
+        }
     }
 }
 
-class Payed extends Payment{
-    protected override pay() {
-        return  ''
+class Product {
+    id: string
+    name: string
+    price: number
+}
+
+class Cart {
+    products: Product[]
+    delivery: Delivery
+
+    addProduct(product: Product) {
+        this.products.push(product)
     }
+
+    deleteProduct(productId: string) {
+        this.products = this.products.filter(({id}) => id == productId)
+    }
+
+    totalCost() {
+        return this.products.reduce((acc, val) => acc += val['price'], 0)
+    }
+
+    addDelivery(delivery: Delivery) {
+        this.delivery = new Delivery(delivery.date, delivery.address, delivery.shopId)
+        return this.delivery
+    }
+
+    checkout() {
+
+    }
+
 }
