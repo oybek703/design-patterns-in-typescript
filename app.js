0ed9d7a36d80015e4b9825dd03d12233794ecc0b
Yoriqulov Oybek <hhoybek@gmail.com>
2022-11-01 12:15:14 +0500

    method params decorator

--- app.js
+++ app.js
@@ -5,57 +5,28 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
 };
 class UserService {
-    constructor() {
-        this.users = 1000;
-    }
     getUsersInDatabase() {
-        throw new Error('Some "getUsersInDatabase" error!');
+        return this.users;
+    }
+    setUsersInDatabase(num, _) {
+        this.users = num;
     }
 }
 __decorate([
-    Max(100)
-], UserService.prototype, "users", void 0);
-__decorate([
-    Log({ reThrow: true })
-], UserService.prototype, "getUsersInDatabase", null);
-function Max(max) {
-    return (target, propertyKey) => {
-        let value;
-        const setter = (newValue) => newValue > max ? console.log(`Cannot set value more than ${max}`) : value = newValue;
-        const getter = () => value;
-        Object.defineProperty(target, propertyKey, {
-            set: setter,
-            get: getter
-        });
+    __param(0, Positive()),
+    __param(1, Positive())
+], UserService.prototype, "setUsersInDatabase", null);
+function Positive() {
+    return (target, propertyKey, parameterIndex) => {
+        console.log(target);
+        console.log(propertyKey);
+        console.log(parameterIndex);
     };
 }
-const userService = new UserService();
-userService.users = 20;
-console.log(userService.users);
-function Log({ reThrow } = { reThrow: false }) {
-    return (target, propertyKey, descriptor) => {
-        descriptor.enumerable = true;
-        const oldFunc = descriptor.value;
-        descriptor.value = (...args) => __awaiter(this, void 0, void 0, function* () {
-            try {
-                return yield (oldFunc === null || oldFunc === void 0 ? void 0 : oldFunc.apply(target, args));
-            }
-            catch (e) {
-                console.error(`Error occurred in ${propertyKey}`);
-                if (reThrow) {
-                    throw e;
-                }
-            }
-        });
-    };
-}
+const usersService = new UserService();
+usersService.setUsersInDatabase(-15);
+console.log(usersService.getUsersInDatabase());
--- app.ts
+++ app.ts
@@ -4,45 +4,25 @@ interface IUserService {
 }
 
 class UserService implements IUserService {
-    @Max(100)
-    users: number = 1000
+    users: number
 
-    @Log({reThrow: true})
     getUsersInDatabase(): number {
-        throw new Error('Some "getUsersInDatabase" error!')
+        return this.users
+    }
+
+    setUsersInDatabase(@Positive() num: number, @Positive() _?: number): void {
+        this.users = num
     }
 }
 
-function Max(max: number) {
-    return (target: Object, propertyKey: string | symbol) => {
-        let value: number
-        const setter = (newValue: number) => newValue > max ? console.log(`Cannot set value more than ${max}`) : value = newValue
-        const getter = () => value
-        Object.defineProperty(target, propertyKey, {
-            set: setter,
-            get: getter
-        })
+function Positive() {
+    return (target: Object, propertyKey: string | symbol, parameterIndex: number) => {
+        console.log(target)
+        console.log(propertyKey)
+        console.log(parameterIndex)
     }
 }
 
-const userService = new UserService()
-userService.users = 20
-console.log(userService.users)
-
-function Log({reThrow}: { reThrow: boolean } = {reThrow: false}) {
-    return (target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<(...args: any[]) => any>): TypedPropertyDescriptor<(...args: any[]) => any> | void => {
-        descriptor.enumerable = true
-        const oldFunc = descriptor.value
-        descriptor.value = async (...args: any[]) => {
-            try {
-                return await oldFunc?.apply(target, args)
-            } catch (e) {
-                console.error(`Error occurred in ${propertyKey as string}`)
-                if (reThrow) {
-                    throw e
-                }
-            }
-        }
-    }
-}
-
+const usersService = new UserService()
+usersService.setUsersInDatabase(-15)
+console.log(usersService.getUsersInDatabase())
