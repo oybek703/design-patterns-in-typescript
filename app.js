277200a87e63ff39ec2a768744e6d5795dd3bd5f
Yoriqulov Oybek <hhoybek@gmail.com>
2022-11-01 10:40:19 +0500

    decorator factory

--- app.js
+++ app.js
@@ -5,20 +5,46 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-let UserService = class UserService {
-    constructor() {
-        this.users = 1000;
-    }
+// @threeUsers
+// @nullUsers
+let UserService = 
+// @setUsersAdvanced(300)
+class UserService {
     getUsersInDatabase() {
         return this.users;
     }
 };
 UserService = __decorate([
-    threeUsers,
-    nullUsers
+    logUsers(),
+    setUsers(10)
+    // @setUsersAdvanced(300)
 ], UserService);
+function logUsers() {
+    console.log('logUsers => init');
+    return (target) => {
+        console.log('logUsers => run');
+        console.log('Users: ', target.prototype.users);
+    };
+}
 function nullUsers(target) {
     target.prototype.users = 0;
+}
+function setUsers(users) {
+    console.log('setUsers => init');
+    return (target) => {
+        console.log('setUsers => run');
+        target.prototype.users = users;
+    };
+}
+function setUsersAdvanced(users) {
+    return (constructor) => {
+        return class extends constructor {
+            constructor() {
+                super(...arguments);
+                this.users = users;
+            }
+        };
+    };
 }
 function threeUsers(constructor) {
     return class extends constructor {
--- app.ts
+++ app.ts
@@ -1,23 +1,50 @@
-@threeUsers
-@nullUsers
+interface IUserService {
+    users: number
+    getUsersInDatabase: () => number
+}
+
+// @threeUsers
+// @nullUsers
+@logUsers()
+@setUsers(10)
+// @setUsersAdvanced(300)
 class UserService implements IUserService {
-    users = 1000
+    users: number
 
     getUsersInDatabase() {
         return this.users
     }
 }
 
-interface IUserService {
-    users: number
-    getUsersInDatabase: () => number
+function logUsers() {
+    console.log('logUsers => init')
+    return (target: Function) => {
+        console.log('logUsers => run')
+        console.log('Users: ', target.prototype.users)
+    }
 }
 
 function nullUsers(target: Function) {
     target.prototype.users = 0
 }
 
-function threeUsers<T extends new (...args: any[]) => {} >(constructor: T) {
+function setUsers(users: number) {
+    console.log('setUsers => init')
+    return (target: Function) => {
+        console.log('setUsers => run')
+        target.prototype.users = users
+    }
+}
+
+function setUsersAdvanced(users: number) {
+    return <T extends new (...args: any[]) => {}>(constructor: T) => {
+        return class extends constructor {
+            users = users
+        }
+    }
+}
+
+function threeUsers<T extends { new(...args: any[]): {} } >(constructor: T) {
     return class extends constructor {
         users = 3
     }
