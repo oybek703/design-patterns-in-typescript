ff1f8e90eec7ce108c40e010e6ee190842df5b2e
Yoriqulov Oybek <hhoybek@gmail.com>
2022-11-02 10:43:29 +0500

    proxy pattern

--- app.ts
+++ app.ts
@@ -1,30 +1,38 @@
-class KVDatabase {
-    db: Map<string, string> = new Map<string, string>()
+interface PaymentDetails {
+    id: number
+    sum: number
+}
 
-    save(key: string, value: string) {
-        this.db.set(key, value)
+interface IPaymentAPI {
+    getPaymentDetails(id: number): PaymentDetails | undefined
+}
+
+class PaymentAPI implements IPaymentAPI {
+
+    private payments: PaymentDetails[] = [{id: 1, sum: 10000}]
+
+    getPaymentDetails(paymentId: number): PaymentDetails | undefined {
+        return this.payments.find(({id}) => paymentId)
     }
 }
 
-class PersistentDB {
-    savePersistent(data: Object) {
-        console.log(data)
+class PaymentAPIProxy implements IPaymentAPI {
+
+    constructor(private api: PaymentAPI, private userId: number) {
+
+    }
+
+    getPaymentDetails(paymentId: number): PaymentDetails | undefined {
+        if (this.userId === 1) {
+            return this.api.getPaymentDetails(paymentId)
+        }
+        console.log('Trying to get payment details!')
     }
 }
 
-class PersistentDatabaseAdapter extends KVDatabase {
+const proxy1 = new PaymentAPIProxy(new PaymentAPI(), 1)
+console.log(proxy1.getPaymentDetails(1))
 
-    constructor(private database: PersistentDB) {
-        super()
-    }
 
-    override save(key: string, value: string) {
-        this.database.savePersistent({key, value})
-    }
-}
-
-function run(base: KVDatabase) {
-    base.save('key1', 'value1')
-}
-
-run(new PersistentDatabaseAdapter(new PersistentDB()))
+const proxy2 = new PaymentAPIProxy(new PaymentAPI(), 2)
+console.log(proxy2.getPaymentDetails(1))
