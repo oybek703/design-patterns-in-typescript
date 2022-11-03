634f31f9628bc16dfed871764e2f75063cf17c43
Yoriqulov Oybek <hhoybek@gmail.com>
2022-11-03 10:01:36 +0500

    console logger singleton self implementation done

---
+++ src/out/consoleLogger/consoleLogger.ts
@@ -0,0 +1,24 @@
+import {IStreamLogger} from '../../core/handlers/streamLogger.interface'
+
+export class ConsoleLogger implements IStreamLogger {
+    private static instance: ConsoleLogger
+
+    static getInstance() {
+        if (!ConsoleLogger.instance) {
+            ConsoleLogger.instance = new ConsoleLogger()
+        }
+        return ConsoleLogger.instance
+    }
+
+    end() {
+        console.log('Ready!')
+    }
+
+    log(data: any) {
+        console.log(data)
+    }
+
+    error(error: any) {
+        console.error(error)
+    }
+}
