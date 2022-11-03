6b9dbbdf218527f3f07f9b60d785f58c617b986c
Yoriqulov Oybek <hhoybek@gmail.com>
2022-11-03 10:25:14 +0500

    command executor done with template method pattern

--- src/core/executor/command.executor.ts
+++ src/core/executor/command.executor.ts
@@ -1,5 +1,23 @@
-export abstract class CommandExecutor {
-    abstract getParams(...args: any[]): void
-    abstract build(): void
-    abstract spawn(): void
+import {ChildProcessWithoutNullStreams} from 'child_process'
+import {IStreamLogger} from '../handlers/streamLogger.interface'
+import {ICommandExec} from './command.types'
+
+export abstract class CommandExecutor<Input> {
+
+    protected constructor(private logger: IStreamLogger) {}
+
+    async execute() {
+        const input = await this.prompt()
+        const command = this.build(input)
+        const stream = this.spawn(command)
+        this.processStream(stream, this.logger)
+    }
+
+    protected abstract prompt(): Promise<Input>
+
+    protected abstract build(input: Input): ICommandExec
+
+    protected abstract spawn(command: ICommandExec): ChildProcessWithoutNullStreams
+
+    protected abstract processStream(stream: ChildProcessWithoutNullStreams, logger: IStreamLogger): void
 }
